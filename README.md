7e75576c54a6d5c1b35780e723fdfeab13c4a24b
Oleg <tony74740@yandex.ru>
2022-06-15 10:41:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Tested on:
+
+Ubuntu-20.04
+C++17
+gcc 9.3.0 x86_64-linux-gnu
---
+++ main.cpp
@@ -0,0 +1,145 @@
+#include <vector>
+#include <iostream>
+#include <array>
+
+static const unsigned short rowsNum = 6;
+static const unsigned short colsNum = 10;
+
+enum nearLiveCells : unsigned short
+{
+    twoLiveCells = 2,
+    threeLiveCells = 3
+};
+
+std::vector<std::array<std::array<bool, colsNum>, rowsNum>> generations;
+
+void makeFirstGeneration()
+{
+    std::array<std::array<bool, colsNum>, rowsNum> cellsStatuses;
+
+    for(unsigned short i = 0; i < rowsNum; i++)
+    {
+        for(unsigned short j = 0; j < colsNum; j++)
+        {   
+            cellsStatuses[i][j] = false;
+
+            if ((rand() % 10 + 1) > 9) // Random
+                cellsStatuses[i][j] = true;
+        }
+    }
+    generations.push_back(std::move(cellsStatuses));
+}
+
+void printField()
+{
+    for (auto const& rowElem : generations.back())
+    {
+        for (auto const colElem : rowElem)
+        {
+            std::cout << colElem;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void newGenerationProcessing(std::array<std::array<bool, colsNum>, rowsNum>& prevGen)
+{
+    std::array<std::array<bool, colsNum>, rowsNum> currentCellsStatuses;
+
+    unsigned short numOfLiveCellsAround = 0;
+    bool isNumOfLiveCellsAroundCalculated = false;
+
+    for(unsigned short i = 0; i < rowsNum; i++)
+    {
+        for(unsigned short j = 0; j < colsNum; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                numOfLiveCellsAround += prevGen[i + 1][j] + prevGen[i + 1][j + 1] + prevGen[i][j + 1];
+                isNumOfLiveCellsAroundCalculated = true;
+            }
+
+            if (i == 0 && j == colsNum)
+            {
+                numOfLiveCellsAround += prevGen[i][j - 1] + prevGen[i + 1][j - 1] + prevGen[i + 1][j];
+                isNumOfLiveCellsAroundCalculated = true;
+            }
+
+            if (i == rowsNum && j == 0)
+            {
+                numOfLiveCellsAround += prevGen[i - 1][j] + prevGen[i - 1][j + 1] + prevGen[i][j + 1];
+                isNumOfLiveCellsAroundCalculated = true;
+            }
+
+            if (i == rowsNum && j == colsNum)
+            {
+                numOfLiveCellsAround += prevGen[i - 1][j] + prevGen[i - 1][j - 1] + prevGen[i][j - 1];
+                isNumOfLiveCellsAroundCalculated = true;
+            }
+
+            if (i == 0)
+            {
+                numOfLiveCellsAround += prevGen[i][j - 1] + prevGen[i + 1][j - 1]
+                    + prevGen[i + 1][j] + prevGen[i + 1][j + 1] + prevGen[i][j + 1];
+                isNumOfLiveCellsAroundCalculated = true;
+            }
+
+            if (i == rowsNum - 1)
+            {
+                numOfLiveCellsAround += prevGen[i][j - 1] + prevGen[i - 1][j - 1]
+                    + prevGen[i - 1][j] + prevGen[i - 1][j + 1] + prevGen[i][j + 1];
+                isNumOfLiveCellsAroundCalculated = true;
+            }
+
+            if (j == 0)
+            {
+                numOfLiveCellsAround += prevGen[i - 1][j] + prevGen[i - 1][j + 1]
+                    + prevGen[i][j + 1] + prevGen[i + 1][j + 1] + prevGen[i + 1][j];
+                isNumOfLiveCellsAroundCalculated = true;
+            }
+
+            if (j == colsNum - 1)
+            {
+                numOfLiveCellsAround += prevGen[i - 1][j] + prevGen[i - 1][j - 1]
+                    + prevGen[i][j - 1] + prevGen[i + 1][j - 1] + prevGen[i + 1][j];
+                isNumOfLiveCellsAroundCalculated = true;
+                // std::cout << numOfLiveCellsAround;
+            }
+
+            if (!isNumOfLiveCellsAroundCalculated)
+            {
+                numOfLiveCellsAround += prevGen[i][j - 1] + prevGen[i - 1][j - 1] + prevGen[i - 1][j]
+                + prevGen[i - 1][j + 1] + prevGen[i][j + 1] + prevGen[i + 1][j + 1] + prevGen[i + 1][j] 
+                    + prevGen[i + 1][j - 1];
+            }
+            
+            currentCellsStatuses[i][j] = false;
+            if (prevGen[i][j]) 
+            {
+                if (numOfLiveCellsAround == nearLiveCells::twoLiveCells || numOfLiveCellsAround == nearLiveCells::threeLiveCells)
+                    currentCellsStatuses[i][j] = true;
+            }
+            else
+            {
+                if (numOfLiveCellsAround == nearLiveCells::threeLiveCells)
+                    currentCellsStatuses[i][j] = true;
+            }
+
+            numOfLiveCellsAround = 0;
+            isNumOfLiveCellsAroundCalculated = false;
+        }
+    }
+    generations.push_back(std::move(currentCellsStatuses));
+}
+
+int main()
+{
+    makeFirstGeneration();
+    printField();
+    
+    newGenerationProcessing(generations.back());
+    printField();
+
+    return 0;
+}
