022c22478c75f3052ceb676d1cd9811a7b5c7bb4
Vasily <tony74740@yandex.ru>
2022-06-18 00:42:35 +0700

    [*]Use vector<bool> instead of array of bitsets

--- FieldProcessor.cpp
+++ FieldProcessor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 #include "FieldProcessor.h"
 
@@ -21,33 +22,50 @@ FieldProcessor::~FieldProcessor()
 void FieldProcessor::makeFirstGeneration()
 {
     std::array<std::bitset<colsNum>, rowsNum> cellsStatuses;
+    std::vector<bool> firstField(startPos, false);
+
+    bool deadCells = false;
 
     for (unsigned short i = 0; i < rowsNum; i++)
     {
         for (unsigned short j = 0; j < colsNum; j++)
-        {   
-            cellsStatuses[i][j] = false;
-
-            if ((rand() % 10 + 1) > 3) // Random
-                cellsStatuses[i][j] = true;
+        { 
+            firstField.push_back(((rand() % 10 + 1) > 4) ? true : false);
+            
+            if (firstField.back())
+                deadCells = true;
         }
+        firstField.insert(firstField.end(), 2, false);
     }
-    generations.push_back(std::move(cellsStatuses));
+
+    firstField.insert(firstField.end(), colsNum + 1, false);
+
+    generations.push_back(std::move(firstField));
+
+    if (!deadCells)
+    {
+        std::cout << "All cells die. Game over" << std::endl;
+        exit(-1);
+    }
 
     printField();
-    isAllCellsDie();
 }
 
 void FieldProcessor::printField()
 {
-    for (auto const& rowElem : generations.back())
+    std::vector<bool>* curGen = &generations.back();
+    std::stringstream ss;
+
+    for (unsigned short i = startPos; i < endPos; i += 2)
     {
         for (unsigned short j = 0; j < colsNum; j++)
         {
-            std::cout << rowElem[j];
+            ss << std::to_string((*curGen)[i]);
+            i++;
         }
-        std::cout << std::endl;
+        ss << std::endl;
     }
+    std::cout << ss.str();
 }
 
 bool FieldProcessor::setCellStatus(unsigned short numOfLiveCellsAround, bool prevStatus)
@@ -67,86 +85,51 @@ bool FieldProcessor::setCellStatus(unsigned short numOfLiveCellsAround, bool pre
 
 void FieldProcessor::newGenerationProcessing()
 {
-    std::array<std::bitset<colsNum>, rowsNum>* prevGen = &generations.back();
-    std::array<std::bitset<colsNum>, rowsNum> currentCellsStatuses;
+    std::vector<bool>* prevGen = &generations.back();
+    std::vector<bool> currentCellsStatuses(startPos, false);
 
-    unsigned short numOfLiveCellsAround = 0;
+    bool deadCells = false;
 
-    // Calculate corners values
-    numOfLiveCellsAround = (*prevGen)[1][0] + (*prevGen)[1][1] + (*prevGen)[0][1];
-    currentCellsStatuses[0][0] = setCellStatus(numOfLiveCellsAround, (*prevGen)[0][0]);
-
-    numOfLiveCellsAround = (*prevGen)[1][colsNum - 1] + (*prevGen)[1][colsNum - 2] + (*prevGen)[0][colsNum - 2];
-    currentCellsStatuses[0][colsNum - 1] = setCellStatus(numOfLiveCellsAround, (*prevGen)[0][colsNum - 1]);
-
-    numOfLiveCellsAround = (*prevGen)[rowsNum - 2][0] + (*prevGen)[rowsNum - 2][1] + (*prevGen)[rowsNum - 1][1];
-    currentCellsStatuses[rowsNum - 1][0] = setCellStatus(numOfLiveCellsAround, (*prevGen)[rowsNum - 1][0]);
-
-    numOfLiveCellsAround = (*prevGen)[rowsNum - 1][colsNum - 2] + (*prevGen)[rowsNum - 2][colsNum - 2] + (*prevGen)[rowsNum - 2][colsNum - 1];
-    currentCellsStatuses[rowsNum - 1][colsNum - 1] = setCellStatus(numOfLiveCellsAround, (*prevGen)[rowsNum - 1][colsNum - 1]);
-
-    // Calculate left and right values
-    for (unsigned short i = 1; i < rowsNum - 1; i++)
-    {
-        numOfLiveCellsAround = (*prevGen)[i - 1][0] + (*prevGen)[i - 1][1]
-                    + (*prevGen)[i][1] + (*prevGen)[i + 1][1] + (*prevGen)[i + 1][0];
-        currentCellsStatuses[i][0] = setCellStatus(numOfLiveCellsAround, (*prevGen)[i][0]);
-
-        numOfLiveCellsAround = (*prevGen)[i - 1][colsNum - 1] + (*prevGen)[i - 1][colsNum - 2]
-                    + (*prevGen)[i][colsNum - 2] + (*prevGen)[i + 1][colsNum - 2] + (*prevGen)[i + 1][colsNum - 1];
-        currentCellsStatuses[i][colsNum - 1] = setCellStatus(numOfLiveCellsAround, (*prevGen)[i][colsNum - 1]);
-    }
-
-    // Calculate top and bottom values
-    for (unsigned short j = 1; j < colsNum - 1; j++)
-    {
-        numOfLiveCellsAround = (*prevGen)[0][j - 1] + (*prevGen)[1][j - 1]
-                    + (*prevGen)[1][j] + (*prevGen)[1][j + 1] + (*prevGen)[0][j + 1];
-        currentCellsStatuses[0][j] = setCellStatus(numOfLiveCellsAround, (*prevGen)[0][j]);
-
-        numOfLiveCellsAround = (*prevGen)[rowsNum - 1][j - 1] + (*prevGen)[rowsNum - 2][j - 1]
-                    + (*prevGen)[rowsNum - 2][j] + (*prevGen)[rowsNum - 2][j + 1] + (*prevGen)[rowsNum - 1][j + 1];
-        currentCellsStatuses[rowsNum - 1][j] = setCellStatus(numOfLiveCellsAround, (*prevGen)[rowsNum - 1][j]);
-    }
-    
-    // Calculate middle values
-    for (unsigned short i = 1; i < rowsNum - 1; i++)
-    {
-        for (unsigned short j = 1; j < colsNum - 1; j++)
-        {
-            numOfLiveCellsAround = (*prevGen)[i][j - 1] + (*prevGen)[i - 1][j - 1] + (*prevGen)[i - 1][j]
-                + (*prevGen)[i - 1][j + 1] + (*prevGen)[i][j + 1] + (*prevGen)[i + 1][j + 1] + (*prevGen)[i + 1][j] 
-                    + (*prevGen)[i + 1][j - 1];
-            currentCellsStatuses[i][j] = setCellStatus(numOfLiveCellsAround, (*prevGen)[i][j]);
-        }
-    }
-    generations.push_back(std::move(currentCellsStatuses));
-
-    printField();
-    isAllCellsDie();
-    checkSameFieldBefore();
-}
-
-void FieldProcessor::isAllCellsDie() // TODO: Need to test with different rand()
-{
-    for (auto const& rowElem : generations.back())
-    {
-        if (rowElem.any())
-            return;
-    }
-
-    std::cout << "All cells die. Game over" << std::endl;
-    exit(-1);
-}
-
-bool FieldProcessor::compareTwoFields(std::array<std::bitset<colsNum>, rowsNum>& firstField, std::array<std::bitset<colsNum>, rowsNum>& secondField)
-{
-    for (unsigned short i = 0; i < rowsNum; i++)
+    for (unsigned short i = startPos; i < endPos; i += 2)
     {
         for (unsigned short j = 0; j < colsNum; j++)
         {
-            if (firstField[i][j] != secondField[i][j])
+            unsigned short numOfLiveCellsAround = (*prevGen)[i - colsNum - 3] + (*prevGen)[i - colsNum - 2] + (*prevGen)[i - colsNum - 1]
+                + (*prevGen)[i - 1] + (*prevGen)[i + 1] + (*prevGen)[i + colsNum + 1] + (*prevGen)[i + colsNum + 2] + (*prevGen)[i + colsNum + 3];
+            
+            currentCellsStatuses.push_back(setCellStatus(numOfLiveCellsAround, (*prevGen)[i]));
+            i++;
+
+            if (currentCellsStatuses.back())
+                deadCells = true;
+        }
+        currentCellsStatuses.insert(currentCellsStatuses.end(), 2, false);
+    }
+
+    currentCellsStatuses.insert(currentCellsStatuses.end(), colsNum + 1, false);
+
+    generations.push_back(std::move(currentCellsStatuses));
+
+    printField();
+
+    if (!deadCells)
+    {
+        std::cout << "All cells die. Game over" << std::endl;
+        exit(-1);
+    }
+
+    checkSameFieldBefore();
+}
+
+bool FieldProcessor::compareTwoFields(std::vector<bool>& firstField, std::vector<bool>& secondField)
+{
+    for (unsigned short i = startPos; i < endPos; i += 2)
+    {
+        for (unsigned short j = 0; j < colsNum; j++)
+        {
+            if (firstField[i] != secondField[i])
                 return false;
+            i++;
         }
     }
     return true;
--- FieldProcessor.h
+++ FieldProcessor.h
@@ -17,13 +17,16 @@ private:
     static const unsigned short rowsNum = 6;
     static const unsigned short colsNum = 10;
 
-    std::vector<std::array<std::bitset<colsNum>, rowsNum>> generations;
+    std::vector<std::vector<bool>> generations;
+
+    // Because real vector starts with zeroes and ends with zeroes by calculation algorithm
+    static const unsigned short startPos = colsNum + 2 + 1;
+    static const unsigned short endPos = ((colsNum + 2) * (rowsNum + 2)) - colsNum - 2;
 
     bool setCellStatus(unsigned short numOfLiveCellsAround, bool prevStatus);
     void makeFirstGeneration();
     void printField();
-    void isAllCellsDie();
-    bool compareTwoFields(std::array<std::bitset<colsNum>, rowsNum>& firstField, std::array<std::bitset<colsNum>, rowsNum>& secondField);
+    bool compareTwoFields(std::vector<bool>& firstField, std::vector<bool>& secondField);
     void checkSameFieldBefore();
 };
 
