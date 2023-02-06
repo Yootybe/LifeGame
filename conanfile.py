94e4a33b5ce23333473f2db2d118837825ed343f
vdmitriev <vdmitriev@tomsksoft.com>
2023-02-06 16:45:36 +0700

    tsae

--- conanfile.py
+++ conanfile.py
@@ -9,13 +9,6 @@ class LifeGameLib(ConanFile):
     exports_sources = ["FieldProcessor.h", "FieldProcessor.cpp", "CMakeLists.txt"]
 
     def build(self):
-        if self.settings.os == "Linux":
-            os.environ["ANDROID_HOME"] = 
-            os.environ["ANDROID_NDK"] =
-            os.environ["ANDROID_NDK_HOME"] =
-            os.environ["ANDROID_NDK_LATEST_HOME"] =
-            os.environ["ANDROID_NDK_ROOT"] =
-            os.environ["ANDROID_SDK_ROOT"] =
         cmake = CMake(self)
         cmake.configure(source_folder='.')
         cmake.build()
